bf387c459fa9ecbbde5ed46873abd0bb4cbc6c71
Stephen Gruzin <stephengrzn@gmail.com>
2025-08-13 02:34:12 -0700

    Added a loading indicator when navigating through pages

---
+++ src/lib/Navigating.svelte
@@ -0,0 +1,23 @@
+<div></div>
+
+<style>
+    div {
+        position: fixed;
+        z-index: 200;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 5px;
+        background: repeating-linear-gradient(to right, #ee3f3f 0%, #3aa0e9 50%, red 100%);
+        background-size: 200% auto;
+        background-position: 0 100%;
+        animation: gradient 2s infinite;
+        animation-fill-mode: forwards;
+        animation-timing-function: linear;
+    }
+
+    @keyframes gradient { 
+        0%   { background-position: 0 0; }
+        100% { background-position: -200% 0; }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from "$lib/components/ui/sonner/index.js";
+    import { navigating } from '$app/state';
+    import Navigating from '@/Navigating.svelte';
 
 	let { children } = $props();
 </script>
+
+{#if navigating.complete}
+	<Navigating />
+{/if}
 
 <Toaster />
 
